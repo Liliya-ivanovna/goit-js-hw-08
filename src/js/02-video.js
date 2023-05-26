5e220a7ad094328c257797c76008d98b454ad24e
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-05-26 21:56:27 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+const idPlayer = document.getElementById('vimeo-player');
+const player = new Player(idPlayer);
+const onPlay = function() {
+    duration: 61.857
+    percent: 0.049
+    seconds: 3.034
+};
+
+player.on('play', onPlay);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.getElementsByName("email");
+const msgEl = document.getElementsByName("message");
+console.log(emailEl)
+formEl.addEventListener('submit',onFormSubmit);
+//emailEl.addEventListener('input',onTextInput);
+//msgEl.addEventListener('input',onTextInput);
+function onFormSubmit(evt){
+evt.preventDefault();
+console.log('evt')
+evt.currentTarget.reset();
+}
+function onTextInput(evt){
+    const message = evt.currentTarget.value;
+    localStorage.setItem("feedback-form-state",message);
+    console.log(message)
+}
+function savedInputsValue(){
+    const savedMsg = localStorage.getItem("feedback-form-state",message);
+    if(savedMsg){
+        console.log('save')
+        formEl.value =savedMsg;
+
+    }
+}

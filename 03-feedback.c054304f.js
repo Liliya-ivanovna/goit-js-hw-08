248b33d9742edbba4302930ab06e1ec5a41c2fb2
Liliya-ivanovna <123566371+Liliya-ivanovna@users.noreply.github.com>
2023-05-28 07:49:38 +0000

    Deploying to gh-pages from @ Liliya-ivanovna/goit-js-hw-08@439e64eefd5c886f8be9bf809cc0d2235b224259 🚀

--- 03-feedback.c054304f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t=document.getElementsByName("email");console.log(t),e.addEventListener("submit",(function(e){e.preventDefault(),console.log("evt"),e.currentTarget.reset()}))}();
-//# sourceMappingURL=03-feedback.c054304f.js.map

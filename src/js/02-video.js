e86e5e908c866141d05b9b1d4f1156f39b9c9cf5
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-05-29 22:08:37 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,6 +6,11 @@ const KEY_STORAGE = "videoplayer-current-time";
 player.on('timeupdate',throttle(function(data){
     localStorage.setItem(KEY_STORAGE, JSON.stringify(data.seconds));
 }),1000)
+const getTime = localStorage.getItem(KEY_STORAGE); 
+  if(getTime){
+   player.setCurrentTime(JSON.parse(getTime));
+  }; 
+
 player.setCurrentTime()
 .then(function(seconds) {})
 .catch(function(error) {
@@ -16,3 +21,5 @@ player.setCurrentTime()
             break;
     }
 });
+
+    
